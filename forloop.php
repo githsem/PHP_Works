0dd5b169807843bfa8dcaf4d31f9d33b82451237
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-08 23:47:09 +0100

    update

---
+++ forloop.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For Loop</title>
+</head>
+<body>
+    <h2>For Statement</h2>
+    
+    <?php
+
+        //For Loops
+        for($count = 0; $count < 3; $count++){
+            echo '<p>Hello World</p>';
+        }
+        for($count = 0; $count < 10; $count++){
+            echo "<p>The Count is $count</p>";
+        }
+
+    ?>
+    
+</body>
+</html>
