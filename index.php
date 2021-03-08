bfe7b699280586c413d53dacebdd612ad380e02c
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-08 23:12:35 +0100

    update

--- index.php
+++ index.php
@@ -15,8 +15,11 @@
     ?>
     <?php
         $name = 'Hans';
+        $age = 30;
         echo $name;
         echo '<h1> My name is : '.$name. '</h1>';
+        echo '<h1> My age is : '.$age. '</h1>';
+        echo "<h1> New $name</h1> ";
     ?>
 
 </body>
