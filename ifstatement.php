e6c3f7c57f2ab53beb02297656a9c04225c089c7
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-08 23:29:22 +0100

    update

--- ifstatement.php
+++ ifstatement.php
@@ -16,6 +16,17 @@
         else{
             echo '<h3 style="color : red"> You have failed </h3>';
         }
+
+        $grade = 'A';
+        if($grade == 'A'){
+            echo '<h2 style="color : green"> You are SUPERSTAR! </h2>';
+        }
+        elseif ($grade == 'B'){
+            echo '<h2 style="color : blue"> You did WELL! </h2>';
+        }
+        else {
+            echo '<h2 style="color : red"> You have FAILED! </h2>';
+        }
     ?>
 </body>
 </html>
