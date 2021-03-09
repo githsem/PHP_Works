95a44cecc4d99ce3398cc3cbdf213410910fd955
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-09 23:17:32 +0100

    update

--- whiledowhile.php
+++ whiledowhile.php
@@ -4,15 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>While Do While Loop</title>
 </head>
 <body>
-<h2>while Do While Loop</h2>
-    
+    <h2>While Loop</h2>
+        
+        <?php
+        $grade = 0;
+        while($grade< 10){
+            echo '<p> I am less than 10</p>';
+            $grade += 1;
+        }
+
+            
+
+        ?>
+    <h2>Do While Loop</h2>    
     <?php
 
-        //While Do While Loop
-        
+            
 
     ?>
 </body>
