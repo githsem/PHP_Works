21d2261c42a18f24c15d02a84456a7275aeeacde
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-09 23:40:00 +0100

    update

---
+++ stringmanip.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String</title>
+</head>
+<body>
+<h2>String</h2>
+        
+        <?php
+            $phrase1 = "student who came late";
+            $phrase2 = "in class, stand with";
+            echo $phrase1;
+            echo '<hr>';
+            echo $phrase2;
+            echo '<hr>';
+            echo $phrase1.$phrase2;
+
+        ?>
+    
+</body>
+</html>
