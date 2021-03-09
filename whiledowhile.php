afb1b93560e78914951cd8ce9f657b7d72879de4
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-09 23:21:20 +0100

    update

--- whiledowhile.php
+++ whiledowhile.php
@@ -10,18 +10,19 @@
     <h2>While Loop</h2>
         
         <?php
-        $grade = 0;
-        while($grade< 10){
-            echo '<p> I am less than 10</p>';
-            $grade += 1;
-        }
-
-            
-
+            $grade = 0;
+            while($grade< 10){
+                echo '<p> I am less than 10</p>';
+                $grade++;
+            }
+            echo '<p> EXIT</p>';
         ?>
     <h2>Do While Loop</h2>    
-    <?php
-
+        <?php
+            do{
+                echo '<p> I am Do While Loop</p>';
+                $grade++;
+            } while($grade< 10)
             
 
     ?>
