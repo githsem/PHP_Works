aa51335babe4d86df24b801a0062d88509d96010
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-10 23:34:25 +0100

    update

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Follow each Link to the page example</h2>
+    <ul>
+        <li><a href="array.php">Array</a></li>
+        <li><a href="forloop.php">For Loop</a></li>
+        <li><a href="whiledowhile.php">While Loop</a></li>
+        <li><a href="ifstatement.php">If Statement</a></li>
+        <li><a href="switchstatement.php">Switch Statement</a></li>
+        <li><a href="stringmanip.php">String</a></li>
+        <li><a href="datetimemanip.php">Date Time</a></li>
+        <li><a href="functions.php">Functions</a></li>
+    </ul>
