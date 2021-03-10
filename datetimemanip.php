55bd13446c6b7d07126ace1c0113f4b5aca78deb
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-10 22:53:44 +0100

    update

--- datetimemanip.php
+++ datetimemanip.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dite Time</title>
 </head>
 <body>
 <h2>Date Time</h2>
         
         <?php
-           
+            $datenow= getdate();
+            echo $datenow["mday"];
+            echo '<hr>';
+            echo '<hr>';
+            echo '<hr>';
+            echo '<hr>';
+            echo '<hr>';
         ?>
 </body>
 </html>
