b531e48bec7781f09582607ea3af440c8e276ac8
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-10 23:05:53 +0100

    update

---
+++ functions.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Functions</title>
+</head>
+<body>
+<h2>Functions</h2>
+        
+        <?php
+            //Defining a Function
+            function writeMessage(){
+                echo "You are very nice";
+            }
+            writeMessage();
+            echo '<hr>';
+            echo '<hr>';
+            echo '<hr>';
+            echo '<hr>';
+            echo '<hr>';
+        ?>
+    
+</body>
+</html>
