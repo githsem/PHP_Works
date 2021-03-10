41fbb574728018e6d01479e74f1a4ad8ab454755
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-10 23:34:16 +0100

    update

--- array.php
+++ array.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Array</title>
-</head>
-<body>
+<?php include 'includes/header.php' ?>
 <h2>Array</h2>
         
         <?php
