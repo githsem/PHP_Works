93b2e13c7030403f70e7f9fc9fbfa0c7c2e105cf
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-11 02:30:03 +0100

    update

--- index.php
+++ index.php
@@ -6,14 +6,6 @@
         echo 'Hello PHP';
         echo '<hr>';
     ?>
-    <?php
-        $name = 'Hans';
-        $age = 30;
-        echo $name;
-        echo '<h1> My name is : '.$name. '</h1>';
-        echo '<h1> My age is : '.$age. '</h1>';
-        echo "<h1> New $name</h1> ";
-    ?>
     <?php include 'includes/footer.php' ?>
 </body>
 </html>
