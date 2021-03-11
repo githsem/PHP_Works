899ad6206e67b158cbfaea04d13a058032b803e2
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-11 02:23:31 +0100

    update

--- array.php
+++ array.php
@@ -4,22 +4,22 @@
 ?>
 <h2>Array</h2>
         
-        <?php
-            //a variable
-            $num = 3;
+    <?php
+        //a variable
+        $num = 3;
 
-            //an array
-            $numbers = array(1,34,3,54,25,6,77,8,900,10);
+        //an array
+        $numbers = array(1,34,3,54,25,6,77,8,900,10);
 
-            echo $numbers[0];
-            echo '<hr>';
-            echo count($numbers);
-            echo '<hr>';
-            for($count = 0; $count<10;$count++){
-                echo $numbers[$count];
-                echo ',';
-            }
-        ?>
+        echo $numbers[0];
+        echo '<hr>';
+        echo count($numbers);
+        echo '<hr>';
+        for($count = 0; $count<10;$count++){
+            echo $numbers[$count];
+            echo ',';
+        }
+    ?>
 <?php require 'includes/footer.php' ?>            
 </body>
 </html>
