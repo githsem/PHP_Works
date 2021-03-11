915088c3a995d20e81e1b36fb931b26935fa1a5b
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-11 02:59:58 +0100

    update

--- includes/header.php
+++ includes/header.php
@@ -4,22 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
  rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" 
  crossorigin="anonymous">
     <title> <?php echo $title; ?> </title>
 </head>
 <body>
+<div class="container">
     <h2>Follow each Link to the page example</h2>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="array.php">Array</a></li>
-        <li><a href="forloop.php">For Loop</a></li>
-        <li><a href="whiledowhile.php">While Loop</a></li>
-        <li><a href="ifstatement.php">If Statement</a></li>
-        <li><a href="switchstatement.php">Switch Statement</a></li>
-        <li><a href="stringmanip.php">String</a></li>
-        <li><a href="datetimemanip.php">Date Time</a></li>
-        <li><a href="functions.php">Functions</a></li>
+    <ul class="nav nav-pills">
+        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="array.php">Array</a></li>
+        <li class="nav-item"><a class="nav-link" href="forloop.php">For Loop</a></li>
+        <li class="nav-item"><a class="nav-link" href="whiledowhile.php">While Loop</a></li>
+        <li class="nav-item"><a class="nav-link" href="ifstatement.php">If Statement</a></li>
+        <li class="nav-item"><a class="nav-link" href="switchstatement.php">Switch Statement</a></li>
+        <li class="nav-item"><a class="nav-link" href="stringmanip.php">String</a></li>
+        <li class="nav-item"><a class="nav-link" href="datetimemanip.php">Date Time</a></li>
+        <li class="nav-item"><a class="nav-link" href="functions.php">Functions</a></li>
     </ul>
+    
