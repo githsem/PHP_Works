1a892d2677270d8cf0a1e2b2e5b5cd424f184be2
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-23 12:01:19 +0100

    update

--- Pizza_Project/add.php
+++ Pizza_Project/add.php
@@ -5,7 +5,7 @@
     if(isset($_POST['submit'])){
         //check email
         if(empty($_POST['email'])){
-            echo 'An email is required <br/>';
+            $errors['email'] = 'An email is required <br/>';
         }else{
             $email = $_POST['email'];
             if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
@@ -15,7 +15,7 @@
 
         //check title
         if(empty($_POST['title'])){
-            echo 'An title is required <br/>';
+            $errors['title'] = 'An title is required <br/>';
         }else{
             $title = $_POST['title'];
             if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
@@ -25,7 +25,7 @@
 
         //check ingredients
         if(empty($_POST['ingredients'])){
-            echo 'At least one ingredients is required <br/>';
+            $errors['ingredients'] = 'At least one ingredients is required <br/>';
         }else{
             $ingredients = $_POST['ingredients'];
             if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
