72990f5a853e6d5be904d4e16a0b4fd06b6b87c2
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-23 21:42:50 +0100

    update

--- Pizza_Project/index.php
+++ Pizza_Project/index.php
@@ -1,10 +1,6 @@
 <?php
-    //connect to database
-    $conn = mysqli_connect('localhost','hasan','hasan123','pizzadb');
-
-    if(!$conn){
-        echo 'Connection Error : '. mysqli_connect_error();
-    } 
+    
+    include('config/db_connect.php');
 
     //write query for all pizzas
     $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
@@ -33,15 +29,15 @@
 <h4 class="center grey-text">Pizzas!</h4>
 <div class="container">
     <div class="row">
-        <?php foreach($pizzas as $pizza) {?>
+        <?php foreach($pizzas as $pizza) :?>
             <div class="col s6 md3">
                 <div class="card z-depth-0">
                     <div class="card-content center">
                         <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
                         <ul>
-                            <?php foreach(explode(',', $pizza['ingredients']) as $ing) { ?>
+                            <?php foreach(explode(',', $pizza['ingredients']) as $ing) : ?>
                                 <li><?php echo htmlspecialchars($ing); ?></li>
-                            <?php } ?>
+                            <?php endforeach; ?>
                         </ul>
                     </div>
                     <div class="card-action right-align">
@@ -49,7 +45,7 @@
                     </div>
                 </div>
             </div>
-        <?php } ?>
+        <?php endforeach; ?>
     </div>
 </div>
 
