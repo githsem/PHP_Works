ea5a318eb5c696c8f5be354acf0505928b830c6c
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-23 12:10:01 +0100

    update

--- Pizza_Project/add.php
+++ Pizza_Project/add.php
@@ -47,13 +47,13 @@
         <h4 class="center">Add a Pizza</h4>
         <form action="add.php" method ="POST" class="white">
             <label for="">Your Email</label>
-            <input type="text" name="email" value="<?php echo $email ?>">
+            <input type="text" name="email" value="<?php echo htmlspecialchars($email)  ?>">
             <div class="red-text"><?php echo $errors['email']; ?></div>
             <label for="">Pizza Title</label>
-            <input type="text" name="title" value="<?php echo $title ?>">
+            <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
             <div class="red-text"><?php echo $errors['title']; ?></div>
             <label for="">Ingredients (comma seperated): </label>
-            <input type="text" name="ingredients" value="<?php echo $ingredients ?>">
+            <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
             <div class="red-text"><?php echo $errors['ingredients']; ?></div>
             <div class="center">
                 <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
