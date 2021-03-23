9742ca6e81e688098f81d6a92e0b1e158b56758d
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-24 00:40:08 +0100

    update

--- Pizza_Project/details.php
+++ Pizza_Project/details.php
@@ -5,6 +5,21 @@ include('config/db_connect.php');
 // ceheck GET request id param
 if(isset($_GET['id'])){
 
+    $id = mysqli_real_escape_string($conn, $_GET['id']);
+
+    //make sql
+    $sql = "SELECT * FROM pizzas WHERE id = $id";
+
+    //get the query result
+    $result = mysqli_query($conn, $sql);
+
+    //fetch result in array format
+    $pizza = mysqli_fetch_assoc($result);
+
+    mysqli_free_result($result);
+    mysqli_close($conn);
+
+    print_r($pizza);
 }
 
 ?>
@@ -14,7 +29,13 @@ if(isset($_GET['id'])){
 
 <?php include('templates/header.php'); ?>
 
-<h2>DETAILS</h2>
+<div class="container center">
+    <?php if($pizza): ?>
+
+    <?php else: ?> 
+
+    <?php endif; ?>    
+</div>
 
 
 <?php include('templates/footer.php'); ?>
