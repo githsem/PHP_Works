e01e7705b2099c02e6f1e7a14be98878a55d17ca
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-23 21:05:12 +0100

    update

--- Pizza_Project/index.php
+++ Pizza_Project/index.php
@@ -7,7 +7,7 @@
     } 
 
     //write query for all pizzas
-    $sql = 'SELECT title, ingredients, id FROM pizzas';
+    $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
     //make query $ get result
     $result = mysqli_query($conn, $sql);
@@ -29,6 +29,24 @@
 <html lang="en">
 
 <?php include('templates/header.php'); ?>
+
+<h4 class="center grey-text">Pizzas!</h4>
+<div class="container">
+    <div class="row">
+        <?php foreach($pizzas as $pizza) {?>
+            <div class="col s6 md3">
+                <div class="card z-depth-0">
+                    <div class="card-content center">
+                        <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                        <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+</div>
+
+
 <?php include('templates/footer.php'); ?>
     
 
