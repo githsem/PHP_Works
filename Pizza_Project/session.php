ebae5d77d94d17399a788532b58444f34609d230
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-25 00:35:25 +0100

    update

---
+++ Pizza_Project/session.php
@@ -0,0 +1,35 @@
+<?php
+
+//session
+if(isset($_POST['submit'])){
+
+    //cookie for gender
+    setcookie('gender',$_POST['gender'], time() + 86400);
+
+    session_start();
+    $_SESSION['name'] = $_POST['name'];
+    header('Location: index.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+    <input type="text name" name="name">
+    <select name="gender" id="">
+        <option value="male">male</option>
+        <option value="female">female</option>
+    </select>
+    <input type="submit" name="submit" value="submit">
+
+</form>
+    
+</body>
+</html>
