1ad1e0c7b77c61217b21481d0556f7a441f03360
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-24 13:05:54 +0100

    update

---
+++ Pizza_Project/sandbox.php
@@ -0,0 +1,27 @@
+<?php
+
+//session
+if(isset($_POST['submit'])){
+    session_start();
+    $_SESSION['name'] = $_POST['name'];
+    header('Location: index.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+    <input type="text name" name="name">
+    <input type="submit" name="submit" value="submit">
+
+</form>
+    
+</body>
+</html>
