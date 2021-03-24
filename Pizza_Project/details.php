57413062a1db51d7c6c03684efe2545c6883aaa5
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-24 01:02:04 +0100

    update

--- Pizza_Project/details.php
+++ Pizza_Project/details.php
@@ -2,14 +2,16 @@
 
 include('config/db_connect.php');
 
-if(isset($_post['delete'])){
+if(isset($_POST['delete'])){
    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']); 
    $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
 
    if(mysqli_query($conn, $sql)){
        //success
+       header('Location: index.php');
    }else{
        //failure
+       echo 'query error: ' . mysqli_error($conn);
    }
 
 }
