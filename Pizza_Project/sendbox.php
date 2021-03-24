e0c06623e87ca4e7db6915e3e39d235f40d56524
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-24 12:48:52 +0100

    update

--- Pizza_Project/sendbox.php
+++ Pizza_Project/sendbox.php
@@ -1,7 +1,11 @@
 <?php
 
-//ternary operators
-
+//session
+if(isset($_POST['submit'])){
+    session_start();
+    $_SESSION['name'] = $_POST['name'];
+    echo $_SESSION['name'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,6 +16,12 @@
     <title>Document</title>
 </head>
 <body>
+
+<form action="<?php echo $_SERVER['PHP_SELF]' ?>" method="POST">
+    <input type="text name">
+    <input type="submit" name="submit" value="submit">
+
+</form>
     
 </body>
 </html>
