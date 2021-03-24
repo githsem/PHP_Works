05637c0091d309ec2e47a40ae855128fe56d1e2c
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-24 13:52:09 +0100

    update

--- Pizza_Project/templates/header.php
+++ Pizza_Project/templates/header.php
@@ -7,6 +7,9 @@
 
     $name = $_SESSION['name'] ?? 'Guest';
 
+    //get cookie
+    $gender = $_COOKIE['gender'] ?? 'Unknown';
+
 ?>
 
 
@@ -44,6 +47,7 @@
                 <a href="index.php" class=" brand-logo brand-text">Pizza</a>
                 <ul id="nav-mobile" class="right hide-on-small-and-down">
                     <li class="grey-text">Hello <?php echo htmlspecialchars($name); ?></li>
+                    <li class="grey-text">(<?php echo htmlspecialchars($gender); ?>)</li>
                     <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
                 </ul>
             </div>
